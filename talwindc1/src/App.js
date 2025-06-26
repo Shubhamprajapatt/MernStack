2e2ce3fe44be45e8e7b0bc21e45f1c26186d7fb6
Shubham Prajapat <161408379+Shubhamprajapatt@users.noreply.github.com>
2025-06-26 15:22:59 +0530

    Talwind scc

---
+++ talwindc1/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+     <h2 className="bg-green-600">
+ helloo this is talwind class
+     </h2> 
+     <p className="bg-yellow-500">
+      thoda sa pad kr ke ana dost
+     </p>
+    </div>
+  );
+} 
+
+export default App;
